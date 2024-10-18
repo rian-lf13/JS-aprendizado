ca174c22d5f8866b415ba54b04cb09624bacf10a
rian-lf13 <rianlops7@gmail.com>
2024-10-18 18:40:52 -0300

    Exercício de fixação
    Introdução ao console.log e exercício de fixação.

--- Secao1/consolelog.js
+++ Secao1/consolelog.js
@@ -1,1 +1,17 @@
 console.log('olá, mundo!!!');
+console.log('Meu nome é "Rian"');
+console.log(`Tenho 23 anos!`);
+// String= Textos
+console.log(24+5);
+console.log(12.50);
+console.log(1234567);
+// Numbers
+
+console.log('Exercício de fixação(refazendo):')
+
+// Exercicio de fixação
+console.log(`Meu nome é "Rian", sou estudante de ADS do primeiro semestre, faz 1 ano que estou flertando com programação e estou entusiasmado com meus últimos meses. Codei com mais frequência e estou revisando tudo que já aprendi para que possa seguir com o curso e me especializar em JS.`)
+
+console.log('Venho estudando com mais frequência e planejo estagiar daqui há 6 meses. Em 2 anos quero ampliar meu conhecimento na área de maneira expansiva e me fixar no mercado de trabalho de TI, como programador.')
+
+console.log('Espero me tornar o profissional que desejo, ajudar pessoas na área, conhecer pessoas e quem sabe ser referência na profissão. Ainda vou ajudar muito as pessoas ao meu redor!')
