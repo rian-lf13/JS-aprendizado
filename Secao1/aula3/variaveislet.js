0aa5b760d71fa7e5b720159a2f2669863e7ee3ba
rian-lf13 <rianlops7@gmail.com>
2024-10-20 17:42:41 -0300

    Adicionado aula  sobre variável

---
+++ Secao1/aula3/variaveislet.js
@@ -0,0 +1,23 @@
+let nomepai = 'Luis'
+let nomemae = 'Maria'
+
+console.log(nomepai, `nasceu em 1984.`);
+console.log('Em 2000' , nomepai, 'conheceu', nomemae,);
+console.log(nomepai,'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nomepai,'em 2015.');
+console.log('O filho de', nomepai,'chama-se Eduardo.');
+
+/* LET é mais atual que VAR, e isso é uma das coisas que diferenciam uma da outra*/
+
+let nome; //apenas declarada a variavel, mas não comtem nenhum valor.
+let nomep='Qualquer valor'//Foi declarado e inicializado
+console.log(nomep)
+
+//não pode criar variáveis com palavras reservadas
+//Variáveis precisam ter nomes significativos
+//Não pode começar o nome de uma variável com número
+//Para nomes compostos utlilizado canelCase
+
+let nomeCompleto = 'Luis Nascimento Silva'
+
+
