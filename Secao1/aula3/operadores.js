3415f79a2afcfd1543217ee15317afe6b3ab3d94
rian-lf13 <rianlops7@gmail.com>
2024-10-22 20:41:17 -0300

    Revisando e fixando operadores

--- Secao1/aula3/operadores.js
+++ Secao1/aula3/operadores.js
@@ -1,2 +1,23 @@
 // Tipos de operadores( Incremento, Atribuição e Aritméticos.)
 
+//Aritméticos 
+/*
++ = adição/concatenação (-, * e /)
+** = potenciação
+%= resto da divisão
+*/
+
+// Exercício
+
+const num1 = 3;
+const num2 = 2;
+const mult = num1 * num2;
+const num3 = 3;
+
+console.log((mult - 1 ) ** num3 );
+
+const num4 = 5;
+const num5 = '10.8';
+const resultado = parseFloat('10.8');
+
+console.log(num4 * resultado / num1);
