efc5ed7a6d3a05c430f7179610051f8f23a87a17
rian-lf13 <rianlops7@gmail.com>
2024-10-26 13:08:28 -0300

    Revisão de strings e numbers

---
+++ Secao1/aula3/numbers.js
@@ -0,0 +1,20 @@
+//IEEE 754-2008 = padrão de precisão
+
+let num1= 110;
+let num2= 2.51563;
+let num3= 1500
+
+console.log(num1.toString() + num2);//Transforma o número numa string.
+
+console.log(num3.toString(2)); //Representação bínaria do número que está naquela var
+
+console.log(num2.toFixed(2));// "Arredonda" número e especifíca quantas casa decimais irão ser representadas.
+
+console.log(Number.isInteger(num1));// Identifica se o número é inteiro ou não;
+
+console.log(Number.isNaN(num1));//Declara se a conta é verdadeira ou falsa/errada ou certa. Não é uma boa prática de programação.
+
+
+
+
+
--- Secao1/aula3/string.js
+++ Secao1/aula3/string.js
@@ -1,5 +1,26 @@
-let varString= "Meu nome é \"Rian\".";
+//              0123456789...
+let varString= "Meu nome é \"Rian\"";//Uma barra e caractere adiciona um caractere ao texto.
 
 // console.log(varString.charAt(5));
-console.log(varString.replace('Meu', 'Seu'));
-console.log(varString.slice(11,17))
+console.log(varString.replace('Meu', 'Seu'));//Replace= muda a palavra que você deseja.
+
+console.log(varString.slice(11,17));//Slice= mostra os caracteres de acordo com o número que deseja.
+
+console.log(varString.charAt(5));//Ìndica o elemento da posição de cada número indicado.
+
+console.log(varString.concat(' ', 'e', ' ', 'o', ' ', 'seu'));//Faz a concatenação de valores, porém, é mais fácil utilizar template.string.
+
+console.log(varString.indexOf('Rian', 5));//Diz onde começa o índice que a palavra está localizada. É importante escrever igual ao texto.
+
+console.log(varString.lastIndexOf('e', 2));// Começa de trás para frente  a localizar a palavra.
+
+console.log(varString.search(/u/g));// Encontra o índice que deseja. Uso da flag (g)
+console.log(varString.length);//Mostra o tamanho da string.
+
+console.log(varString.toUpperCase);//Mostra o texto em maiúsculo.
+
+console.log(varString.slice(3, 8));// Serve para fatear o texto.
+
+console.log(varString.split(' ', 1));// Divide o texto.
+
+console.log(varString.toLowerCase);//Texto em letras minúsculas.
