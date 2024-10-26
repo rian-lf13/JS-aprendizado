85433846bdc2efbc34688e13e6ef13b729bd351d
rian-lf13 <rianlops7@gmail.com>
2024-10-26 19:56:53 -0300

    Aula math

---
+++ Secao1/aula3/math.js
@@ -0,0 +1,36 @@
+//Alguns exemplos de como usar
+let num1= 9.56483;
+// let num2= Math.floor(num1);
+// console.log(num2);
+/*No exemplo acima é usado Math.floor para arredondar o número 
+ignorando as casas decimais.*/
+
+// let num2= Math.ceil(num1);
+// console.log(num2);
+/*Já nesse outro exemplo o número é arredondado para uma casa acima.
+ Ex.: De 9.56318 vai para 10*/
+
+// let num2= Math.round(num1);
+// console.log(num2);
+/*Nesse exemplo o número tambem é arredondado só que para de maneira automatica para cima ou para baixo
+Ex.: 9.51>10 ou 9.49<9*/
+
+// console.log(Math.max(1,2,3,-10,1500,326,2222,154));
+/*Nesse exemplo revela o maior número dentro de uma sequência.*/
+
+
+// console.log(Math.min(1,2,3,-10,1500,326,2222,154));
+/*Nesse exemplo revela o menor número dentro de uma sequência.*/
+
+
+// console.log(Math.random());
+//É gerado um número aleatório entre 0 e 1
+
+// console.log(Math.PI)
+//Serve para pegar o número PI 
+
+// console.log(Math.pow(2,11))
+//Eleva um número a outro número
+
+
+
