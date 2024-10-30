22a8532c6bd088fb54d0c938bca3e5d4a34af343
rian-lf13 <rianlops7@gmail.com>
2024-10-30 10:35:00 -0300

    Array (Básico)
    Introdução ao array

--- Secao1/aula3/numbers.js
+++ Secao1/aula3/numbers.js
@@ -8,11 +8,11 @@ console.log(num1.toString() + num2);//Transforma o número numa string.
 
 console.log(num3.toString(2)); //Representação bínaria do número que está naquela var
 
-console.log(num2.toFixed(2));// "Arredonda" número e especifíca quantas casa decimais irão ser representadas.
+console.log(num2.toFixed(2)); // "Arredonda" número e especifíca quantas casa decimais irão ser representadas.
 
-console.log(Number.isInteger(num1));// Identifica se o número é inteiro ou não;
+console.log(Number.isInteger(num1)); // Identifica se o número é inteiro ou não;
 
-console.log(Number.isNaN(num1));//Declara se a conta é verdadeira ou falsa/errada ou certa. Não é uma boa prática de programação.
+console.log(Number.isNaN(num1)); //Declara se a conta é verdadeira ou falsa/errada ou certa. Não é uma boa prática de programação.
 
 
 
