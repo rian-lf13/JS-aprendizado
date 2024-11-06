ddb430a8e311609649979e715ef3637c070ef0d0
rian-lf13 <rianlops7@gmail.com>
2024-11-06 17:10:13 -0300

    Objetos
    Introdução aos objetos, para começar a incrementar exercícios com o que foi aprendido em aulas passadas.

---
+++ Secao1/aula7/objetos_basico.js
@@ -0,0 +1,47 @@
+// const array = [1,2,3];
+// array.push(4);
+// array[0]= 10;
+// console.log(array);
+
+// const pessoa1 ={
+//     nome: 'Caio',
+//     sobrenome: 'Nascimento',
+//     idade: 24
+// };
+
+// / console.log(pessoa1.sobrenome);
+
+
+/*function criaPessoa(nome, sobrenome, idade) {
+     return{nome, sobrenome,idade
+     }
+};
+
+const pessoa1= criaPessoa('Luiz', 'Nascimento', 16);
+const pessoa2= criaPessoa('Larissa', 'Rodrigues', 61);
+const pessoa3= criaPessoa('Carlos', 'Santos', 31);
+
+console.log(pessoa1.idade, pessoa2.nome, pessoa3.sobrenome);
+
+// nesse exemplo é usado função para retornar as informações guardadas em objetivos e alterada por const.
+*/
+
+/*const pessoa1 = {
+    nome:'Luiz',
+    sobrenome:'Rocha',
+    idade: 54,
+
+    diz () {
+        console.log(`${this.nome} ${this.sobrenome} fala que tem ${this.idade}anos.`);
+    },
+
+    incremento () {
+    this.idade++
+    }
+};
+
+pessoa1.diz();
+pessoa1.incremento();
+pessoa1.diz();
+pessoa1.incremento();*/
+
