a86c17a16b03bbbf38b2d48bab6f3957e0ea57ad
rian-lf13 <rianlops7@gmail.com>
2024-11-09 12:52:01 -0300

    Continuação do ex.

--- Secao1/aula7/funcoes.js
+++ Secao1/aula7/funcoes.js
@@ -36,3 +36,20 @@ const raizQ = n => n ** 0.5;
     
 console.log(raizQ(9));    
 console.log(raizQ(16));//Isso é uma função simplificada.
+
+Exercício de fixação
+
+function meuEscopo () {
+    const form = document.querySelector('.form');
+
+    // form.onsubmit = function(evento){
+    //     evento.preventDefault()
+    //     alert(1);
+    //     console.log('Foi enviado!')
+    };  
+
+    let contador = 1;
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
+        console.log('Form não foi enviado.')
+    };
---
+++ Secao1/aula8/js/ex.js
@@ -0,0 +1,20 @@
+function meuEscopo () {
+    const form= document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
+
+    function recebeEventoForm(evento){
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso= form.querySelector('.peso');
+        const altura= form.querySelector('.altura');
+
+        console.log(nome.ariaValueMax, sobrenome, peso, altura);
+
+    };
+
+    form.addEventListener('submit', recebeEventoForm);
+};
