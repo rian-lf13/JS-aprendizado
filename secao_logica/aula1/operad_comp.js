aaa45228ed1ff3d3dea6c5110bdaf23ede68298e
rian-lf13 <rianlops7@gmail.com>
2024-12-06 20:22:52 -0300

    Operadores de comparação
    Feito alguns exemplos para fixação.

---
+++ secao_logica/aula1/operad_comp.js
@@ -0,0 +1,23 @@
+/*
+Operadores de comparação
+> maior que 
+>= maior que ou igual a
+< menor que 
+<= menor que ou igual a
+== igualdade (verifica valor) **Não é muito recomend. utilizar esse...
+=== igualdade estrita (verifica valor e tipo)
+!= Diferente (verifica valor)
+!== Diferente estrito (verifica valor e tipo)
+*/
+
+console.log(10>5); /* nesse caso, o resultado será um valor booleano (true, false)*/
+const w= 10>=5;
+console.log(w)
+
+let a = 10!==5;
+console.log(a)
+
+const num1= 10;
+const num2= 5;
+const result= num1 >= num2;
+console.log(result);
