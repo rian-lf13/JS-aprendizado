f962c450a881924afd0cc54bcc90fe87bf512c8f
rian-lf13 <rianlops7@gmail.com>
2024-12-09 21:37:57 -0300

    revisão de operadores lógicos
    Apenas a definição e como pode ser usado

---
+++ secao_logica/operad_logicos.js
@@ -0,0 +1,13 @@
+/*
+Operadores lógicos
+&& -> and -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> or -> ou -> Somente uma expressão precisa ser verdadeira para retornar true
+! -> NOT -> não -> Representa negação. Ex.: se o resultado de console.log retornar(!true), vai retornar como FALSE
+*/
+
+const expressaoAnd = true && true && true && false;/*exemplo &&*/
+const expressaoOr = true || false;/*exemplo ||*/
+console.log(expressaoOr); 
+console.log(!true);/*exemplo de ! */
+
+
